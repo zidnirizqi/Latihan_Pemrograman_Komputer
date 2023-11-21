27173b6d03deff4c47576716d79f8fe71cc23a19
Zidni Rizqi <117433786+zidnirizqi@users.noreply.github.com>
2023-11-21 11:58:43 +0700

    Add files via upload

---
+++ Mahasiswa/src/main/java/com/mycompany/mahasiswa/Mahasiswa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mahasiswa;
+
+/**
+ *
+ * @author ACER
+ */
+public class Mahasiswa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
